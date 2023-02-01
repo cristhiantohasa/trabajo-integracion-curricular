c574d0fa68edfdeb44260750fc4747f604ec9490
Cristhian Tohasa <cristhian.tohasa@epn.edu.ec>
2023-02-01 14:17:01 -0500

    Se realiza el post

--- api/main.py
+++ api/main.py
@@ -1,38 +1,70 @@
-from flask import Flask;
-from flask import Request;
-import xmltodict;
-import json;
-import pyodbc;
+from flask import Flask
+from flask import Request
+import xmltodict
+import json
+import pyodbc
 
-try:
-    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=SERVER1;DATABASE=Registro_Civil_EPN;UID=sa;PWD=smile')
-    print("Conexión exitosa.")
-except Exception as ex:
-    print("Error durante la conexión: {}".format(ex))
-finally:
-    connection.close()  # Se cerró la conexión a la BD.
-    print("La conexión ha finalizado.")
-
-
-app = Flask( __name__ );
+app = Flask( __name__ )
 
 #Insertar Datos
 @app.route('/salvadatos/insertarDatos', methods=["POST"])
-def obtenerDatos():
+def insertarDatos():
 
     with open("resources/datos.xml", 'r') as xmlDatos:
-        jsonDatos = xmltodict.parse(xmlDatos.read());
+        jsonRoot = xmltodict.parse(xmlDatos.read())
 
-    jsonDatos = json.dumps(jsonDatos);
-    jsonDatos = json.loads(jsonDatos);
+    jsonRoot = json.dumps(jsonRoot)
+    jsonRoot = json.loads(jsonRoot)
 
+    jsonDatos = jsonRoot['root']
 
+    jsonPersona = jsonDatos['persona']
+    jsonContacto = jsonDatos['contacto']
+    jsonDatosPersonales = jsonDatos['datosPesonales']
+    jsonEmpelador = jsonDatos['empleador']
+    jsonHistorial = jsonDatos['historial']
+
+    cedula = jsonPersona['cedula']
+    nombres = jsonPersona['nombres']
+    apellidos = jsonPersona['apellidos']
+    sexo = jsonPersona['sexo']
+    condicionCiudadano = jsonPersona['condicionCiudadano']
+    fechaNacimiento = jsonPersona['fechaNacimiento']
+    lugarNacimiento = jsonPersona['lugarNacimiento']
+    nacionalidad = jsonPersona['nacionalidad']
+    estadoCivil = jsonPersona['estadoCivil']
+    codigoDactilar = jsonPersona['codigoDactilar']
+
+    domicilio = jsonContacto['domicilio']
+    callesDomicilio = jsonContacto['callesDomicilio']
+    numeroCasa = jsonContacto['numeroCasa']
+    telefono = jsonContacto['telefono']
+    correoElectronico = jsonContacto['correoElectronico']
+
+    try:
+        
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=SERVER1;DATABASE=Registro_Civil_EPN;UID=sa;PWD=smile')
+        print("Conexión exitosa...")
+
+        qrRegistroCivil = 'EXEC sp_insertarDatos \'' + cedula + '\', \'' + nombres + '\', \'' + apellidos + '\', \'' + sexo + '\', \'' + condicionCiudadano + '\', \'' + fechaNacimiento + '\', \'' + lugarNacimiento + '\', \'' + nacionalidad + '\', \'' + estadoCivil + '\', \'' + codigoDactilar + '\', \'' + domicilio + '\', \'' + callesDomicilio + '\', \'' + numeroCasa + '\', \'' + telefono + '\', \'' + correoElectronico + '\''
+        print(qrRegistroCivil)
+
+        cursorInsert = connection.cursor()
+
+        cursorInsert.execute(qrRegistroCivil)
+        cursorInsert.commit()
+
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        connection.close()  # Se cerró la conexión a la BD.
+        print("La conexión ha finalizado.")
 
     return jsonDatos;
 
 # Obtener Datos
 @app.route('/salvadatos/obtenerDatos', methods=["GET"])
-def insertarDatos():
+def obtenerDatos():
 
     print("Get");
 
